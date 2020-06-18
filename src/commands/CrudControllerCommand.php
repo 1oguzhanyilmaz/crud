d9a3f36df8f7a92a290d5b867b7abf3546f4b318
Oğuzhan Yılmaz <45510538+1oguzhanyilmaz@users.noreply.github.com>
2020-06-18 23:14:46 +0300

    fix namespace problem

--- src/commands/CrudControllerCommand.php
+++ src/commands/CrudControllerCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Commands;
+namespace Oy\Commands;
 
 use Illuminate\Console\GeneratorCommand;
 use Illuminate\Support\Str;
--- src/commands/CrudViewCommand.php
+++ src/commands/CrudViewCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Commands;
+namespace Oy\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
